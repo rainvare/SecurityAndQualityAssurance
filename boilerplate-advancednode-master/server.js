f917b5bfef912d16fdfb9dd4a67fb1585b6c176f
rainvare <49820865+rainvare@users.noreply.github.com>
2020-06-12 21:02:50 -0300

    Update server.js

--- boilerplate-advancednode-master/server.js
+++ boilerplate-advancednode-master/server.js
@@ -79,6 +79,20 @@ app.route("/").get((req, res) => {
   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
 });
 
+  
+  function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+};
+  
+ app
+ .route('/profile')
+ .get(ensureAuthenticated, (req,res) => {
+    res.render(process.cwd() + '/views/pug/profile');
+ });
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
 });
