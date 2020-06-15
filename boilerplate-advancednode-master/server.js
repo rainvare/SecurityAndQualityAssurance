462eaa4a3d80114aab4fa47ebe374f1060bf9e2b
rainvare <49820865+rainvare@users.noreply.github.com>
2020-06-15 18:41:32 -0300

    Update server.js

--- boilerplate-advancednode-master/server.js
+++ boilerplate-advancednode-master/server.js
@@ -1,89 +1,101 @@
 "use strict";
+
+//variables
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
-const pug = require('pug');
-const session = require('express-session');
-const passport = require('passport');
+const session = require("express-session");
+const passport = require("passport");
 const ObjectID = require("mongodb").ObjectID;
 const mongo = require("mongodb").MongoClient;
 const LocalStrategy = require('passport-local');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+//For FCC testing purposes
+fccTesting(app);
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//uso de pug template
-app.set('view engine', 'pug');
+//set pug template
+app.set("view engine", "pug");
 
-//passport session
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-}));
+//passport middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
+//iniciar passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Database conect
+//BASE DE DATOS MONGO
 
-mongo.connect(process.env.DATABASE, (err, db) => {
-  if (err) {
-    console.log("Database error: " + err);
-  } else {
+mongo.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },(err, db) => {
+  if (err) { 
+    console.log('Database error: ' + err);
+  } else { 
     console.log("Successful database connection");
+
+    //uso de passport para autenticar user DB al hacer login
+    passport.use(
+      new LocalStrategy((username, password, done) => {
+        db.collection("users").findOne({ username: username }, (err, user) => {
+          console.log("User " + username + " attempted to log in.");
+          if (err) return done(err);
+          if (!user) return done(null, false);
+          if (password !== user.password) return done(null, false);
+
+          return done(null, user);
+        });
+      })
+    );
+   
     
-//serializacion  deserialización 
+    //serialization and app.listen van dentro de mongodb
+    //serializar y deserializar
     passport.serializeUser((user, done) => {
-      done(null, user._id);
+      done(user._id);
     });
 
     passport.deserializeUser((id, done) => {
-      db.collection("users").findOne({ _id: new ObjectID(id) }, (err, doc) => {
-        done(null, doc);
+      db.collection('users').findOne(
+       {_id: new ObjectID(id)},
+         (err, doc) => {
+            done( doc);
+         }
+       );
+    });
+    
+
+    //pug template
+    app.route("/").get((req, res) => {
+      res.render(process.cwd() + "/views/pug/index", {
+        title: "Hello",
+        message: "Please login",
+        //formulario de inicio de sesión
+        showLogin: true,
+        //formulario de registro
+        showRegistration: true
       });
     });
-  }
-  
-  passport.use(new LocalStrategy(
-  function(username, password, done) {
-    db.collection('users').findOne({ username: username }, function (err, user) {
-      console.log('User '+ username +' attempted to log in.');
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (password !== user.password) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
-  
-//autenticar login 
-  function ensureAuthenticated(req, res, next) {
+    
+   // funcion middleware para verificacion de login de user
+    function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect("/");
-};
-//middleware autenticado e implementado en profile
- app
- .route('/profile')
- .get(ensureAuthenticated, (req,res) => {
-    res.render(process.cwd() + '/views/pug/profile');
- });  
-   //llamar pug template y renderizar
-  //formulario de login
-  app.route("/").get((req, res) => {
-        res.render(process.cwd() + "/views/pug/index", {
-          title: 'Home Page ',
-          message: "Please login",
-          showLogin: true,
-        });
-      });
-
-  //local passport autenticar
-      app
+  res.redirect('/');
+}; 
+    
+    //ruta de inicio de sesión - profile home
+app
         .route("/login")
         .post(
           passport.authenticate("local", { failureRedirect: "/" }),
@@ -91,30 +103,63 @@ mongo.connect(process.env.DATABASE, (err, db) => {
             res.redirect("/profile");
           }
         );
- 
-  //render profile
-  app.route("/profile").get(ensureAuthenticated, (req, res) => {
+
+    //middleware dirige a profile
+      app.route("/profile").get(ensureAuthenticated, (req, res) => {
         res.render(process.cwd() + "/views/pug/profile", {
           username: req.user.username,
         });
       });
-
-//llamar logout route to home page
-app.route("/logout").get((req, res) => {
-        req.logout();
-        res.redirect("/");
-      });
-  
-//404 
- app.use((req, res, next) => {
-        res.status(404).type("text").send("Not Found");
-      }); 
-
-  
-  //listen port
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+    
+    //logout 
+    app.route('/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/');
 });
+      
+    //registro de usuario
+    app.route('/register')
+  .post((req, res, next) => {
+    db.collection('users').findOne({ username: req.body.username }, function(err, user) {
+      if (err) {
+        next(err);
+      } else if (user) { 
+        res.redirect('/');
+      } else {
+        db.collection('users').insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, user);
+            }
+          }
+        )
+      }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+  
    
+    //página faltante - error 404
+    app.use((req, res, next) => {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+    //listen PORT
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on port " + process.env.PORT);
+    });
+  }
 });
