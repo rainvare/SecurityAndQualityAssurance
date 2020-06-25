6e9b2f4ea605705e69ef466aea0c52a9eff5c55b
rainvare <49820865+rainvare@users.noreply.github.com>
2020-06-25 19:02:50 -0300

    Update server.js

--- boilerplate-socialauth-gomix/server.js
+++ boilerplate-socialauth-gomix/server.js
@@ -1,103 +1,134 @@
-'use strict';
-
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const session     = require('express-session');
-const mongo       = require('mongodb').MongoClient;
-const passport    = require('passport');
-
+"use strict";
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const mongo = require("mongodb").MongoClient;
+const passport = require("passport");
+const GitHubStrategy = require("passport-github").Strategy;
+const http = require("http").Server(app);
+const io = require("socket.io")(http); //challenge 16
+const cors = require("cors");
+app.use(cors());
 
 fccTesting(app); //For FCC testing purposes
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
 mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) {
-        console.log('Database error: ' + err);
-    } else {
-        console.log('Successful database connection');
-      
-        app.use(session({
-          secret: process.env.SESSION_SECRET,
-          resave: true,
-          saveUninitialized: true,
-        }));
-        app.use(passport.initialize());
-        app.use(passport.session());
-      
-        function ensureAuthenticated(req, res, next) {
-          if (req.isAuthenticated()) {
-              return next();
-          }
-          res.redirect('/');
-        };
+  if (err) {
+    console.log("Database error: " + err);
+  } else {
+    console.log("Successful database connection");
 
-        passport.serializeUser((user, done) => {
-          done(null, user.id);
-        });
+    app.use(
+      session({
+        secret: process.env.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: true
+      })
+    );
+    passport.use(
+      new GitHubStrategy(
+        {
+          clientID: process.env.GITHUB_CLIENT_ID,
+          clientSecret: process.env.GITHUB_CLIENT_SECRET,
+          callbackURL:
+            "https://fcc-adv-node-and-express14-16.glitch.me/auth/github/callback"
+        },
+        function(accessToken, refreshToken, profile, cb) {
+          console.log(profile);
+          //Database logic here with callback containing our user object (challenges 14-16)
+          db.collection("socialusers").findAndModify(
+            { id: profile.id },
+            {},
+            {
+              $setOnInsert: {
+                id: profile.id,
+                name: profile.displayName || "John Doe",
+                photo: profile.photos[0].value || "",
+                email: profile.emails[0].value || "No public email",
+                created_on: new Date(),
+                provider: profile.provider || ""
+              },
+              $set: {
+                last_login: new Date()
+              },
+              $inc: {
+                login_count: 1
+              }
+            },
+            { upsert: true, new: true },
+            (err, doc) => {
+              return cb(null, doc.value);
+            }
+          );
+          //end of dbcollection find and modify
+        }
+      )
+    );
 
-        passport.deserializeUser((id, done) => {
-            db.collection('socialusers').findOne(
-                {id: id},
-                (err, doc) => {
-                    done(null, doc);
-                }
-            );
-        });
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-      
-        /*
-        *  ADD YOUR CODE BELOW
-        */
-      
-       app.route( '/auth/github' ) //challenge 14: Implementation of Social Authentication
-          .get( passport.authenticate( 'github' , { failureRedirect: '/' } ), ( req,res ) => {
-            res.redirect( '/profile' );
-          });    
+    function ensureAuthenticated(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect("/");
+    }
 
-        app.route( '/auth/github/callback' ) //challenge 14: Implementation of Social Authentication
-          .get( passport.authenticate( 'github', { failureRedirect: '/' } ), ( req,res ) => {
-            res.redirect( '/profile' );
-          });   
-      
-      
-      
-      
-      
-        /*
-        *  ADD YOUR CODE ABOVE
-        */
-      
-      
-        app.route('/')
-          .get((req, res) => {
-            res.render(process.cwd() + '/views/pug/index');
-          });
+    passport.serializeUser((user, done) => {
+      done(null, user.id);
+    });
 
-        app.route('/profile')
-          .get(ensureAuthenticated, (req, res) => {
-               res.render(process.cwd() + '/views/pug/profile', {user: req.user});
-          });
+    passport.deserializeUser((id, done) => {
+      db.collection("socialusers").findOne({ id: id }, (err, doc) => {
+        done(null, doc);
+      });
+    });
 
-        app.route('/logout')
-          .get((req, res) => {
-              req.logout();
-              res.redirect('/');
-          });
+    app
+      .route("/auth/github") //challenge 14: Implementation of Social Authentication
+      .get(passport.authenticate("github"));
 
-        app.use((req, res, next) => {
-          res.status(404)
-            .type('text')
-            .send('Not Found');
-        });
-      
-        app.listen(process.env.PORT || 3000, () => {
-          console.log("Listening on port " + process.env.PORT);
-        });  
-}});
+    app
+      .route("/auth/github/callback") //challenge 14: Implementation of Social Authentication
+      .get(
+        passport.authenticate("github", { failureRedirect: "/" }),
+        (req, res) => {
+          res.redirect("/profile");
+        }
+      );
+    // end 
+    //routes
+
+    app.route("/").get((req, res) => {
+      res.render(process.cwd() + "/views/pug/index");
+    });
+
+    app.route("/profile").get(ensureAuthenticated, (req, res) => {
+      res.render(process.cwd() + "/views/pug/profile", { user: req.user });
+    });
+
+    app.route("/logout").get((req, res) => {
+      req.logout();
+      res.redirect("/");
+    });
+
+    app.use((req, res, next) => {
+      res
+        .status(404)
+        .type("text")
+        .send("Not Found");
+    });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on port " + process.env.PORT);
+    });
+  }
+});
