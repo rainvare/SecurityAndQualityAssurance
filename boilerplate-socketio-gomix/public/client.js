f2c2c7d28aaf33e44008e8a6073236af39db0d1d
rainvare <49820865+rainvare@users.noreply.github.com>
2020-07-07 16:39:09 -0300

    Update client.js

--- boilerplate-socketio-gomix/public/client.js
+++ boilerplate-socketio-gomix/public/client.js
@@ -1,8 +1,31 @@
 $( document ).ready(function() {
-   /*global io*/
-var socket = io();
   
+  /*global io*/
+var socket = io(); //challenge 17
    
+  
+  socket.on('user count', function(data){ //challenge 18 conteo de usuarios
+  console.log(data);
+});
+  
+   //challenge 21: Announce New Users
+  socket.on('user', function(data){
+    $('#num-users').text(data.currentUsers+' users online');
+    var message = data.name;
+    if(data.connected) {
+      message += ' has joined the chat.';
+    } else {
+      message += ' has left the chat.';
+    }
+    $('#messages').append($('<li>').html('<b>'+ message +'<\/b>'));
+  });
+  
+  socket.on('chat message', function(data){
+    console.log(data);
+    $('#messages').append($('<li>').text(data.name + ': ' +data.message));
+  });
+  
+  
   // Form submittion with new message in field with id 'm'
   $('form').submit(function(){
     var messageToSend = $('#m').val();
